36a66834d2e708718182ac0082010877db45d1b9
emydorado <145810874+emydorado@users.noreply.github.com>
2024-02-09 17:53:27 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const studentGrades = [
+	{ grade: '95' },
+	{ grade: '87' },
+	{ grade: '71' },
+	{ grade: '55' },
+	{ grade: '90' },
+	{ grade: '10' },
+];
+
+function compare(a, b) {
+	if (grade > 90 < 100) {
+		return 'a';
+	}
+}
